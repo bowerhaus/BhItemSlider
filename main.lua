38ed14edd8f55a16cb85e05ff7aa659880348d5e
Andy Bower <bower@bowerhaus.eu>
2012-09-12 20:16:03 +0100

    Compatible with first version of BhListbox

--- main.lua
+++ main.lua
@@ -1,17 +1,7 @@
-BhDemoShield.new(true)
-
 local listItems={"Apple", "Banana", "Cabbage", "Dog", "Elephant", "Fox", "Giraffe", "Hen"}
 
 local tahomaFont=TTFont.new("Tahoma.ttf", 30)
 
-local list=BhListBox.new(40)
-stage:addChild(list)
-for k,v in pairs(listItems) do
-	local text=TextField.new(tahomaFont, v)
-	list:addChild(text)
-end
-list:resetScroll()
-list:setPosition( 700, 100)
 
 local slider=BhItemSlider.new(80, 40, false)
 stage:addChild(slider)
